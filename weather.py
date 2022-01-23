a96c17b46d4efdc687aa703276a5cbe1b8e339f0
Nicholas Kissaun <31455235+Tephrite@users.noreply.github.com>
2022-01-23 22:00:59 +0000

    Added and trained weather functions

--- weather.py
+++ weather.py
@@ -23,3 +23,30 @@ def get_temp(sentence):
     
     print("The current temperature in {l} is {t:.0f} degrees".format(l=location, t=temp))
     return("The current temperature in {l} is {t:.0f} degrees".format(l=location, t=temp))
+
+def get_coldest_temp(sentence):
+    location = get_location(sentence)
+    api_data = get_api(location)
+    temp = ((api_data['main']['temp_min']) - 273.15)
+    
+    print("The lowest it will be today in {l} will be {t:.0f} degrees".format(l=location, t=temp))
+    return("The lowest it will be today in {l} will be {t:.0f} degrees".format(l=location, t=temp))
+
+def get_hottest_temp(sentence):
+    location = get_location(sentence)
+    api_data = get_api(location)
+    temp = ((api_data['main']['temp_max']) - 273.15)
+    
+    print("The highest it will be today in {l} will be {t:.0f} degrees".format(l=location, t=temp))
+    return("The highest it will be today in {l} will be {t:.0f} degrees".format(l=location, t=temp))
+
+def get_weather(sentence):
+    location = get_location(sentence)
+    api_data = get_api(location)
+    weather = ((api_data['weather'][0])['description'])
+    
+    if weather == "clear sky":
+        weather = "clear skies"
+    
+    print("It is currently {w} in {l}".format(l=location, w=weather))
+    return("It is currently {w} in {l}".format(l=location, w=weather))
