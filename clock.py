fc13f9bf4dbbc980e7303a2e9d7d7eb9fdc6a026
Nicholas Kissaun <31455235+Tephrite@users.noreply.github.com>
2022-01-30 23:18:55 +0000

    Added snooze alarm and timers

--- clock.py
+++ clock.py
@@ -2,10 +2,12 @@ from datetime import datetime
 import pytz
 import dateutil.parser
 
-from Learning.Conversation.entity_recognition_training import get_location
+from Learning.Conversation.entity_recognition_training import get_location, extract_time
 
-text = "set an alarm for 2:12"
+text = "set a timer for 1 minute"
 local = "Brighton"
+
+alarms = []
 timers = []
 
 # Get time from location
@@ -26,12 +28,6 @@ def get_time(sentence):
         
         print("The time in "+location+" is " + country_time.strftime("%I:%M %p"))
         return ("The time in "+location+" is " + country_time.strftime("%I:%M %p"))
-
-#Extract time from sentence
-def extract_time(sentence):
-    datetime = dateutil.parser.parse(sentence, fuzzy=True)
-    return datetime
-    
     
 def get_timezone(country):
     for tz in pytz.common_timezones:
@@ -40,25 +36,26 @@ def get_timezone(country):
 
 def get_time_difference(sentence):
     print("THIS IS TIME DIFFERENCE")  
+    
 
 #Makes a check to see if a timer has been set for the current time
-#TODO: Double check that this works with multiple alarm
-def check_timers():
+def check_alarms():
     current_hour = int(datetime.now().strftime("%I"))
     current_min = int(datetime.now().strftime("%M"))
     
-    for alarm in timers:
+    for alarm in alarms:
         if current_hour == alarm[0]:
             if current_min == alarm[1]:
                 print("RING DING DING")
-                timers.pop(0)
     
 def set_alarm(sentence):
-    h = extract_time(sentence).hour
-    m = extract_time(sentence).minute
+    time = extract_time(sentence)
+    h = time[0]
+    m = time[1]
+    
     alarm = [h,m]
     
-    timers.append(alarm)
+    alarms.append(alarm)
     
     if h < 9:
         hour = "0"+str(h)
@@ -73,11 +70,79 @@ def set_alarm(sentence):
     return("Alarm set for "+hour+":"+min)
     
 def stop_alarm(sentence):
-    print("STOP ALARM")
+    alarms.pop(0)
+    
+    return("Alarm deactivated")
+
+def snooze_alarm(sentence):
+    try:
+        time = extract_time(sentence)
+        hour = time[0]
+        minute = time[1]
+    except:
+        hour = int(datetime.now().strftime("%I"))
+        minute = int(datetime.now().strftime("%M"))
+        
+        
+    for alarm in alarms:
+        print("for")
+        if hour == alarm[0]:
+            if minute == alarm[1]:
+                if minute > 54:
+                    # TODO: Account for if its 23:55
+                    alarm = [hour+1, minute-55]
+                else:
+                    alarm = [hour, minute+5]
+    
+    return("Alarm Snoozed")
     
 def set_timer(sentence):
-    print("SET TIMER")
+    time = extract_time(sentence)
+    h = time[0]
+    m = time[1]
+    
+    current_h = int(datetime.now().strftime("%I"))
+    current_m = int(datetime.now().strftime("%M"))
+    
+    timer_h = current_h + h
+    timer_m = current_m + m
+    
+    if timer_m > 54:
+        timer_h += 1
+        timer_m -= 55
+    
+    timer = [timer_h, timer_m]
+    timers.append(timer)
+    
+    return_string = "Timer set for "
+    
+    if h == 0:
+        return_string += str(m)
+        if m > 0:
+            return_string += " minutes"
+        else:
+            return_string += " minute"
+    if m == 0:
+        return_string += str(h)
+        if h > 0:
+            return_string += " hours"
+        else:
+            return_string += " hour"
+        
+    return return_string
     
 def stop_timer(sentence):
-    print("STOP TIMER")
+    timers.pop(0)
     
+    return("Timer deactivated")
+    
+#Makes a check to see if a timer has been set for the current time
+def check_timers():
+    current_hour = int(datetime.now().strftime("%I"))
+    current_min = int(datetime.now().strftime("%M"))
+    
+    for timer in timers:
+        if current_hour == timer[0]:
+            if current_min == timer[1]:
+                print("RING DING DING")
+                
